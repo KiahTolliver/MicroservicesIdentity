5c73e03487313c61539efa0cc01b434d95669c75
Kiah Imani <kiah@c-trax.com>
2024-03-06 12:34:59 -0800

    Built out EventAPI

---
+++ Services/MicroservicesIdentity.Services.EventAPI/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AutoMapper;
+using MicroservicesIdentity.Services.EventAPI.Models;
+using MicroservicesIdentity.Services.EventAPI.Models.Dto;
+
+namespace MicroservicesIdentity.Services.EventAPI
+{
+	public class MappingConfig
+	{
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<EventDto, Event>();
+                config.CreateMap<Event, EventDto>();
+            });
+
+            return mappingConfig;
+        }
+
+    }
+}
+
